c5183aaaa4c1ace0a126e4eb33dd460f8d0716d2
thomas-vaiarella <thomasvaiarella@gmail.com>
2019-06-11 21:17:02 -0400

    Added placholder Home page

--- src/App.js
+++ src/App.js
@@ -5,66 +5,15 @@ import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import Register from './register/Register';
 import Profile from './profile/Profile';
 import Search from './search/Search';
+import Home from './home/Home';
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            cocktailSearchText: "",
-            shownCocktails: [],
-            modalShown: false,
-            modalCocktailId: -1
-        }
-    }
-
-    updateResults = (results) => {
-        results.then(
-            objs => this.setState({
-                shownCocktails: objs.drinks
-            })
-
-        );
-
-        console.log(this.state)
-    };
-
-    updateSearch = (event) => {
-        let search = event.target.value;
-
-        fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`)
-            .then(response => this.updateResults(response.json()));
-
-        this.setState({
-            cocktailSearchText: event.target.value
-        })
-    };
-
-    showInformation = (id) => {
-        this.setState({
-            modalShown: true,
-            modalCocktailId: id
-        });
-    };
-
-    hideModal = () => {
-        this.setState({
-            modalShown: false,
-            modalCocktailId: -1
-        });
-    };
-
-
-    render() {
-        return (
-            <Router>
-                <Route exact path="/login" component={Login}/>
-                <Route exact path="/register" component={Register}/>
-                <Route exact path="/profile" component={Profile}/>
-                <Route exact path="/search" component={Search}/>
-            </Router>
-        );
-    }
-}
+const App = () =>
+    <Router>  
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/register" component={Register}/>
+        <Route exact path="/profile" component={Profile}/>
+        <Route exact path="/search" component={Search}/>
+    </Router>
 
 export default App;
---
+++ src/home/Home.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Home = () =>
+    <h1>home works!</h1>
+
+export default Home
